3f29339a545fd1b80055e3169a3581df5f39b3de
Fateme <fatemejaberi2002@gmail.com>
2023-12-28 18:09:54 +0330

    displaying title of movies object using ORM.

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    movies = Movie.objects.all()
+    # get the titles:
+    output = ", ".join([m.title for m in movies])
+    return HttpResponse(output)
